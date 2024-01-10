4de702a5d921d1186c3bcd71c1cb4b6aaf5579f7
Rafael Rodrigo Silveira <rafaelrodrigosilveira@hotmail.com>
2024-01-10 18:53:43 -0300

    v0.8
    Adicionado carros na pista apos inicializar o game.

--- main.gd
+++ main.gd
@@ -8,3 +8,19 @@ var score2 = 0
 
 func _ready():
 	pass 
+
+# INSTANCIA NOVOS CARROS
+func _on_horacarrorapido_timeout():
+	var carrorapido = novocarro.instantiate()
+	add_child(carrorapido)
+	carrorapido.position.x = -10
+	carrorapido.position.y = prapido[randi() % prapido.size()] # sorteia um carro
+	carrorapido.linear_velocity = Vector2(randf_range(700,710),0) # movimenta carro da esquerda para direita
+
+
+func _on_horacarrodevagar_timeout():
+	var carrodevagar = novocarro.instantiate()
+	add_child(carrodevagar)
+	carrodevagar.position.x = -10
+	carrodevagar.position.y = pdevagar[randi() % pdevagar.size()] # sorteia um carro
+	carrodevagar.linear_velocity = Vector2(randf_range(300,310),0) # movimenta carro da esquerda para direita
