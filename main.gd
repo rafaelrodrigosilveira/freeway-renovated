583f92a164fca4eaf53a5084ab77893151f9c29b
Rafael Rodrigo Silveira <rafaelrodrigosilveira@hotmail.com>
2024-01-12 07:38:36 -0300

    v0.15
    colocado variaveis nas velocidades dos carros

--- main.gd
+++ main.gd
@@ -2,6 +2,13 @@ extends Node
 const novocarro = preload("res://carros_rigid_body_2d.tscn")
 var pdevagar = [600,544,438,324,384,216,160]
 var prapido = [488,272,104]
+# Velocidades possiveis dos carros
+@export var velRapidaMinima = 700
+@export var velRapidaMinima2 = 710
+@export var velDevagarMinima = 300
+@export var velDevagarMinima2 = 310
+# PARAMETROS INICIAIS DOS JOGADORES
+@export var fase = 1
 var score1 = 0
 var score2 = 0
 
@@ -17,7 +24,7 @@ func _on_horacarrorapido_timeout():
 	add_child(carrorapido)
 	carrorapido.position.x = -10
 	carrorapido.position.y = prapido[randi() % prapido.size()] # sorteia um carro
-	carrorapido.linear_velocity = Vector2(randf_range(700,710),0) # movimenta carro da esquerda para direita
+	carrorapido.linear_velocity = Vector2(randf_range(velRapidaMinima,velRapidaMinima2),0) # movimenta carro da esquerda para direita
 
 
 func _on_horacarrodevagar_timeout():
@@ -25,7 +32,7 @@ func _on_horacarrodevagar_timeout():
 	add_child(carrodevagar)
 	carrodevagar.position.x = -10
 	carrodevagar.position.y = pdevagar[randi() % pdevagar.size()] # sorteia um carro
-	carrodevagar.linear_velocity = Vector2(randf_range(300,310),0) # movimenta carro da esquerda para direita
+	carrodevagar.linear_velocity = Vector2(randf_range(velDevagarMinima,velDevagarMinima2),0)# movimenta carro da esquerda para direita
 
 # EFETUA PONTUACAO PARA PLAYER1
 func _on_player_pontua():
