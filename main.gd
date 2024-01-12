456e30c7e2e11c7cfc2076f0857bb1047c2427b2
Rafael Rodrigo Silveira <rafaelrodrigosilveira@hotmail.com>
2024-01-12 07:41:02 -0300

    v0.15
    corrigido script

--- main.gd
+++ main.gd
@@ -3,12 +3,10 @@ const novocarro = preload("res://carros_rigid_body_2d.tscn")
 var pdevagar = [600,544,438,324,384,216,160]
 var prapido = [488,272,104]
 # Velocidades possiveis dos carros
-@export var velRapidaMinima = 700
-@export var velRapidaMinima2 = 710
-@export var velDevagarMinima = 300
-@export var velDevagarMinima2 = 310
-# PARAMETROS INICIAIS DOS JOGADORES
-@export var fase = 1
+var velRapidaMinima = 700
+var velRapidaMinima2 = 710
+var velDevagarMinima = 300
+var velDevagarMinima2 = 310
 var score1 = 0
 var score2 = 0
 
