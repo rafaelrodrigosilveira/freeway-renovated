391135e0c1c8c117089927f2ffdecdd56ea3264e
Rafael Rodrigo Silveira <rafaelrodrigosilveira@hotmail.com>
2024-01-13 16:08:53 -0300

    v1.0
    Corrigido bugs:
    - faz sumir as galinhas apos vitoria
    - 5 pontos acaba o jogo
    - melhorado a interface inicial

--- main.gd
+++ main.gd
@@ -9,6 +9,7 @@ var velDevagarMinima = 300
 var velDevagarMinima2 = 310
 var score1 = 0
 var score2 = 0
+@export var scoreMaximo = 5
 
 
 func _ready():
@@ -34,32 +35,34 @@ func _on_horacarrodevagar_timeout():
 
 # EFETUA PONTUACAO PARA PLAYER1
 func _on_player_pontua():
-	if score1 < 10:
+	if score1 < scoreMaximo:
 		score1 +=1
 		$ponto_AudioStreamPlayer2D.play()
 		$placar1.text = str(score1)
-	if score1 >=10:
+	if score1 >=scoreMaximo:
 		$Button.show() # exibe botao de reinicio
 		$tema_AudioStreamPlayer2D3.stop()
 		$resultado.text = "Player 1 Ganhou!"
 		$ganhou_AudioStreamPlayer2D2.play()
 		$horacarrodevagar.stop()
 		$horacarrorapido.stop()
-
+		$Player.queue_free()
+		$Player2.queue_free()
 
 func _on_player_2_pontua_2():
-	if score2 < 10:
+	if score2 < scoreMaximo:
 		score2 +=1
 		$ponto_AudioStreamPlayer2D.play()
 		$placar2.text = str(score2)
-	if score2 >=10:
+	if score2 >=scoreMaximo:
 		$Button.show() # exibe botao de reinicio
 		$tema_AudioStreamPlayer2D3.stop()
 		$resultado.text = "Player 2 Ganhou!"
 		$ganhou_AudioStreamPlayer2D2.play()
 		$horacarrodevagar.stop()
 		$horacarrorapido.stop()
-
+		$Player.queue_free()
+		$Player2.queue_free()
 
 func _on_button_pressed():
 	get_tree().reload_current_scene()
